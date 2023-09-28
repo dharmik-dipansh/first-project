697884a74d84959fb8e87d8003990a965a743217
dharmik-dipansh <146363892+dharmik-dipansh@users.noreply.github.com>
2023-09-28 21:20:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import Head from 'next/head'
 
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Head>
+        <title>Hello Next.js + Tailwind</title>
+      </Head>
+      <h1 className="text-4xl font-bold text-center">Hello, World!</h1>
+    </div>
+  )
+}
